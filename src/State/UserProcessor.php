36c33c701ccc3b321caf47b1945fe23fb556f508
hvgochr <charrade.hugo@gmail.com>
2024-01-06 17:30:01 +0100

    Add UserProcessor.php

---
+++ src/State/UserProcessor.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\User;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserProcessor implements ProcessorInterface
+{
+    public function __construct(#[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')] private ProcessorInterface $persistProcessor, private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
+    {
+        $this->chiffrerMotDePasse($data, $data->getPlainPassword());
+        $this->persistProcessor->process($data, $operation, $uriVariables, $context);
+    }
+
+    public function chiffrerMotDePasse(User $user, ?string $plainPassword): void
+    {
+        if ($plainPassword == null) return;
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+        $user->setPassword($hashedPassword);
+    }
+}
