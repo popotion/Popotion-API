d1476c919100a4fd10220de88cd07c487f4e0b21
hvgochr <charrade.hugo@gmail.com>
2024-01-23 18:35:42 +0100

    Update RecipeProcessor: auto-creation of ingredient if not exists & auto creation of composition

--- src/State/RecipeProcessor.php
+++ src/State/RecipeProcessor.php
@@ -4,7 +4,10 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\Entity\Composition;
 use App\Entity\Recipe;
+use App\Entity\Ingredient;
+use App\Repository\IngredientRepository;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
@@ -13,7 +16,8 @@ class RecipeProcessor implements ProcessorInterface
 
     public function __construct(
         #[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')] private ProcessorInterface $persistProcessor,
-        private Security $security
+        private Security $security,
+        private IngredientRepository $ingredientRepository
     ) {
     }
 
@@ -21,6 +25,23 @@ class RecipeProcessor implements ProcessorInterface
     {
         $user = $this->security->getUser();
         $this->setAuthor($data, $user);
+
+        foreach ($data->getCompositionsData() as $compositionData) {
+            $ingredient = $this->ingredientRepository->findOneByName($compositionData->ingredientName);
+            if (!$ingredient) {
+                $ingredient = new Ingredient();
+                $ingredient->setName($compositionData->ingredientName);
+                $this->ingredientRepository->save($ingredient);
+            }
+
+            $composition = new Composition();
+            $composition->setIngredient($ingredient);
+            $composition->setQuantity($compositionData->quantity);
+            $composition->setUnit($compositionData->unit);
+            $composition->setRecipe($data);
+            $data->addComposition($composition);
+        }
+
         $this->persistProcessor->process($data, $operation, $uriVariables, $context);
     }
 
