ff4bca448ea68e5bf5d2c0a92bb0360abc85868d
hvgochr <charrade.hugo@gmail.com>
2024-01-23 19:41:26 +0100

    Add autocreation of categories if not exists

--- src/State/RecipeProcessor.php
+++ src/State/RecipeProcessor.php
@@ -4,9 +4,11 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\Entity\Category;
 use App\Entity\Composition;
 use App\Entity\Recipe;
 use App\Entity\Ingredient;
+use App\Repository\CategoryRepository;
 use App\Repository\IngredientRepository;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -17,7 +19,8 @@ class RecipeProcessor implements ProcessorInterface
     public function __construct(
         #[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')] private ProcessorInterface $persistProcessor,
         private Security $security,
-        private IngredientRepository $ingredientRepository
+        private IngredientRepository $ingredientRepository,
+        private CategoryRepository $categoryRepository
     ) {
     }
 
@@ -26,7 +29,22 @@ class RecipeProcessor implements ProcessorInterface
         $user = $this->security->getUser();
         $this->setAuthor($data, $user);
 
-        foreach ($data->getCompositionsData() as $compositionData) {
+        $this->createIngredientsAndSetCompositions($data, $data->getCompositionsData());
+
+        $this->createCategories($data->getCategoryNames(), $data);
+
+        $this->persistProcessor->process($data, $operation, $uriVariables, $context);
+    }
+
+    public function setAuthor(Recipe $recipe, mixed $author): void
+    {
+        if ($author == null) return;
+        $recipe->setAuthor($author);
+    }
+
+    public function createIngredientsAndSetCompositions(Recipe $recipe, array $compositionsData): void
+    {
+        foreach ($compositionsData as $compositionData) {
             $ingredient = $this->ingredientRepository->findOneByName($compositionData->ingredientName);
             if (!$ingredient) {
                 $ingredient = new Ingredient();
@@ -38,16 +56,21 @@ class RecipeProcessor implements ProcessorInterface
             $composition->setIngredient($ingredient);
             $composition->setQuantity($compositionData->quantity);
             $composition->setUnit($compositionData->unit);
-            $composition->setRecipe($data);
-            $data->addComposition($composition);
+            $composition->setRecipe($recipe);
+            $recipe->addComposition($composition);
         }
-
-        $this->persistProcessor->process($data, $operation, $uriVariables, $context);
     }
 
-    public function setAuthor(Recipe $recipe, mixed $author): void
+    public function createCategories(array $categoryNames, Recipe $recipe): void
     {
-        if ($author == null) return;
-        $recipe->setAuthor($author);
+        foreach ($categoryNames as $categoryName) {
+            $category = $this->categoryRepository->findOneByName($categoryName);
+            if (!$category) {
+                $category = new Category();
+                $category->setName($categoryName);
+                $this->categoryRepository->save($category);
+            }
+            $recipe->addCategory($category);
+        }
     }
 }
