451c39e582d49040ac3e3f148473d88f75e01d68
hvgochr <charrade.hugo@gmail.com>
2024-01-25 16:40:19 +0100

    Rm unused imports on User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,7 +19,6 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
