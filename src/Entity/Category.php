c875b88dbd6a21d2d44573aaf2cb586f2582080b
hvgochr <charrade.hugo@gmail.com>
2024-01-25 16:11:50 +0100

    Add recipe:read groups to Category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -13,6 +13,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 #[ApiResource(
@@ -43,6 +44,7 @@ class Category
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['recipe:read'])]
     private ?string $name = null;
 
     #[ApiProperty(writable: false)]
